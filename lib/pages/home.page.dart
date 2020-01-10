37915059f04a379563c3545633d9195536d79f8c
Raphael Vasconcelos <raphael.vasconcelos@somagrupo.com.br>
2020-01-10 15:54:31 -0300

    Adicionando animações para cada status

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,5 +1,6 @@
 import 'package:eggs/blocs/app.bloc.dart';
 import 'package:eggs/widgets/action-buttons.widget.dart';
+import 'package:eggs/widgets/eggs.widget.dart';
 import 'package:eggs/widgets/progress.widget.dart';
 import 'package:eggs/widgets/type-buttons.widget.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,10 @@ class HomePage extends StatelessWidget {
               height: 10,
             ),
             Progress(),
+            SizedBox(
+              height: 10,
+            ),
+            Eggs(),
           ],
         ),
       ),
